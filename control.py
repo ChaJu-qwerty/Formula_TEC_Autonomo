7d098e913eb18c2da2c70a0ab84194e3a8521115
ChaJu-qwerty <zukodechaju@gmail.com>
2025-09-23 20:59:06 -0700

    modificaciones a control.py

--- control.py
+++ control.py
@@ -1,1 +1,16 @@
-#COSO DE CONTROL
+from gpiozero import AngularServo
+from time import sleep
+servoPin = 14
+
+servo = AngularServo(servoPin, min_angle = 0, max_angle = 180, min_pulse_width = 0.5/1000, max_pulse_width = 2.5/1000)
+
+def set_angle(angle):
+    servo.angle = angle
+    sleep(1)
+
+try:
+    while True:
+        angle = int(input("Ingresa un angulo de 0 a 180: "))
+        set_angle(angle)
+except KeyboardInterrupt:
+    print("programa interrumpido")

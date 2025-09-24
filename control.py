9581ce0e72857a76c56a73d75a5d7a9b6f6d7cd4
ChaJu-qwerty <zukodechaju@gmail.com>
2025-09-23 21:57:23 -0700

    cambios para la vibracion del servo

--- control.py
+++ control.py
@@ -1,16 +1,31 @@
 from gpiozero import AngularServo
+from gpiozero.pins.pigpio import PiGPIOFactory
 from time import sleep
+
+# Usar PiGPIO para PWM más estable
+factory = PiGPIOFactory()
 servoPin = 23
 
-servo = AngularServo(servoPin, min_angle = 0, max_angle = 180, min_pulse_width = 0.5/1000, max_pulse_width = 2.5/1000)
+servo = AngularServo(servoPin, 
+                    min_angle=0, 
+                    max_angle=180,
+                    min_pulse_width=0.0005,   # 0.5ms
+                    max_pulse_width=0.0025,   # 2.5ms
+                    frame_width=0.02,         # 20ms periodo
+                    pin_factory=factory)
 
 def set_angle(angle):
-    servo.angle = angle
-    sleep(1) #tiempo para que el servo pueda llegar a la posicion 
+    # Limitar ángulo y redondear
+    angle = max(0, min(180, angle))
+    rounded_angle = round(angle)
+    
+    servo.angle = rounded_angle
+    sleep(0.5)  # Tiempo suficiente para movimiento
 
 try:
     while True:
-        angle = int(input("Ingresa un angulo de 0 a 180: "))
+        angle = int(input("Ingresa un ángulo de 0 a 180: "))
         set_angle(angle)
 except KeyboardInterrupt:
-    print("programa interrumpido")
+    print("Programa interrumpido")
+    servo.detach()  # Liberar el servo
